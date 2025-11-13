aee4eec293c1a02583768329d0467e697eac3c34
JoelGwC <101189997+JoelGwC@users.noreply.github.com>
2025-11-13 10:16:46 +0800

    firebase initialisation

---
+++ y/firebase-config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+
+// TODO: Replace the following with your app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB0XjpqmXA7wab9EBNvzo-8abHxjJnDqdk",
+    authDomain: "localtest-0327.firebaseapp.com",
+    databaseURL: "https://localtest-0327-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "localtest-0327",
+    storageBucket: "localtest-0327.firebasestorage.app",
+    messagingSenderId: "619374002435",
+    appId: "1:619374002435:web:c66e8326a677a71bf22936"
+};
+
+const app = initializeApp(firebaseConfig);
